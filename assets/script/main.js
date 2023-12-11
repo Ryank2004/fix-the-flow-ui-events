40f915c1162e7d5d22dce9d9e4adf52f1c6e2dcc
Ryank2004 <ryankorver04@gmail.com>
2023-12-11 17:43:21 +0100

    button 3,4,5

--- assets/script/main.js
+++ assets/script/main.js
@@ -17,16 +17,52 @@ frontend.addEventListener('click', function() {
 })
 
 // button 2
-let design = document.querySelector('a:nth-of-type(2)');
-console.log("design", design);
+let design = document.querySelector('a:nth-of-type(2)')
+console.log("design", design)
 
-document.addEventListener('keydown', keyX);
+document.addEventListener('keydown', keyX)
 
 function keyX(event) {
 
-    if (event.key = 'x') {
+    if (event.key === 'x') {
         design.classList.toggle('backgroundColor');
     }
 }
 
 // button 3
+let and = document.querySelector('a:nth-of-type(3)')
+
+and.addEventListener('dblclick', function(){
+  and.classList.toggle('rotate')
+})
+
+// button 4
+let development = document.querySelector('a:nth-of-type(4)')
+
+document.addEventListener('keydown', keyCDown);
+document.addEventListener('keyup', keyCUp);
+
+function keyCDown(event) {
+
+    if (event.key === 'c') {
+        development.classList.add('backgroundColor');
+    }
+}
+function keyCUp(event) {
+
+    if (event.key === 'c') {
+      development.classList.remove('backgroundColor');
+    }
+}
+console.log("keyup" ,keyCUp)
+
+// button 5
+let sprint5 = document.querySelector('a:nth-of-type(5)');
+
+sprint5.addEventListener('mouseover', function() {
+    sprint5.classList.add('hoverSize');
+});
+
+sprint5.addEventListener('mouseout', function() {
+    sprint5.classList.remove('hoverSize');
+});
