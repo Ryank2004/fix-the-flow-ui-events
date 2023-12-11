c77e8bf5c428813bcebb6f67d6cc0cf6e0f7d9c4
Ryank2004 <ryankorver04@gmail.com>
2023-12-11 11:58:26 +0100

    button 1&2 sorryy

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,27 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+
+// button 1
+let frontend = document.querySelector('a:nth-of-type(1)')
+console.log("frontend",frontend)
+
+frontend.addEventListener('click', function() {
+  frontend.classList.toggle('sizeBigger')
+})
+
+// button 2
+let design = document.querySelector('a:nth-of-type(2)');
+console.log("design", design);
+
+document.addEventListener('keydown', keyX);
+
+function keyX(event) {
+
+    if (event.key = 'x') {
+        design.classList.toggle('backgroundColor');
+    }
+}
+
+// button 3
